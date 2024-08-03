b1280108ff824573edb3c1202e4f3381ca7a2a32
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-08-03 15:36:34 -0300

    Text test e imágenes de prueba

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+from datetime import datetime, UTC
+from pathlib import Path
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -20,3 +22,21 @@ def PHONE_TESTER() -> str: return os.getenv("PHONE_TESTER")
 @pytest.fixture
 def wapchita(WAP_API_KEY: str, WAP_DEVICE_ID: str) -> Wapchita:
     return Wapchita(tkn=WAP_API_KEY, device=WAP_DEVICE_ID)
+
+@pytest.fixture
+def path_data() -> Path:
+    path_data_ = Path(__file__).parent / "data"
+    path_data_.mkdir(exist_ok=True)
+    return path_data_
+
+@pytest.fixture
+def TEXT_TEST() -> str:
+    return f"Simple text message {datetime.now(tz=UTC)}"
+
+@pytest.fixture
+def PATH_IMG_PNG_TEST(path_data: Path) -> Path:
+    return path_data / "michis.png"
+
+@pytest.fixture
+def PATH_IMG_JPEG_TEST(path_data: Path) -> Path:
+    return path_data / "michis.jpeg"
--- tests/test_client.py
+++ tests/test_client.py
@@ -4,5 +4,6 @@ from requests import Response
 
 from wapchita.client import Wapchita
 
-def test_send_message(wapchita: Wapchita, PHONE_TESTER: str) -> Response:
-    return wapchita.send_message(phone=PHONE_TESTER, message="asd")
+
+def test_send_message_text(wapchita: Wapchita, PHONE_TESTER: str, TEXT_TEST: str) -> Response:
+    return wapchita.send_message(phone=PHONE_TESTER, message=TEXT_TEST)
