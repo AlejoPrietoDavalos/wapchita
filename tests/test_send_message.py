30460ced04508bd43d6ccf3c8f01f96d4c1f1cfc
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-08-03 15:41:01 -0300

    test_send_message_text

--- tests/test_send_message.py
+++ tests/test_send_message.py
@@ -4,6 +4,6 @@ from requests import Response
 
 from wapchita.client import Wapchita
 
-
 def test_send_message_text(wapchita: Wapchita, PHONE_TESTER: str, TEXT_TEST: str) -> Response:
-    return wapchita.send_message(phone=PHONE_TESTER, message=TEXT_TEST)
+    r = wapchita.send_message(phone=PHONE_TESTER, message=TEXT_TEST)
+    assert r.status_code == 201
