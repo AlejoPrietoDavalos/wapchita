03999d1c5b0ef36c77b03cec1c0a29b450192ae0
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-08-10 12:16:56 -0300

    Se mueve modelos secundarios a un folder separado

--- wapchita/models/chats.py
+++ wapchita/models/chats.py
@@ -7,50 +7,9 @@ from pydantic import BaseModel, Field
 from wapchita.models.device import WapDevice
 from wapchita.models._extras._webhook import WapMeta, WapLinks, MsgType
 from wapchita.models.user import WapUser
+from wapchita.models._extras._chats import WapchitaMedia, WebhookStatus
 
-
-class WebhookStatus(BaseModel):
-   status: str
-   webhookSyncedAt: Optional[str]
-
-class WapchitaAudioMeta(BaseModel):
-    duration: Optional[int] = None
-    hasPreview: bool
-
-class WapchitaAudioStats(BaseModel):
-    downloads: int
-
-class WapchitaAudioPreview(BaseModel):
-    image: Optional[str] = None
-
-class WapchitaAudioLinks(BaseModel):
-    resource: str
-    download: str
-    chat: str
-    contact: str
-    message: str
-
-class WapchitaMedia(BaseModel):
-    id: str
-    message: str
-    chat: str
-    flow: str                               # Literal["in"] #TODO: Habrá "out"?
-    status: str                             # Literal["available"]
-    caption: Optional[str] = None
-    type: str                               # audio
-    size: int
-    mime: str                               # audio/mp3
-    extension: str                          # mp3
-    format: Optional[str | dict] = None     # ptt
-    source: Optional[str] = None            # "upload"
-    uploadFileId: Optional[str] = None      # Es el ID del elemento subido.
-    filename: str
-    createdAt: datetime
-    expiresAt: datetime
-    meta: WapchitaAudioMeta
-    stats: WapchitaAudioStats
-    preview: WapchitaAudioPreview
-    links: WapchitaAudioLinks
+__all__ = ["WapChat", "WapChats"]
 
 
 class WapChat(BaseModel):
