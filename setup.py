bf1f13b0d467734d404dede88d83684a1275c6a0
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-08-21 03:01:11 -0300

    0.1.1: async_get_chats_history y get_chat_details

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="wapchita",
-    version="0.1.0",
+    version="0.1.1",
     author="Alejo Prieto Dávalos",
     author_email="alejoprietodavalos@gmail.com",
     packages=find_packages(),
