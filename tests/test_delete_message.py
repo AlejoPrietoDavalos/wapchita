924e67dc2bde3e9a4662decdccb6039c202ec7c4
EmannuelDM <ema.dmaccio@gmail.com>
2024-08-21 19:23:17 -0300

    correccion de test_delete_message

--- tests/test_delete_message.py
+++ tests/test_delete_message.py
@@ -1,20 +1,17 @@
 from datetime import datetime, UTC
 
-import pytest
-
 from constants import WAP_API_KEY, WAP_DEVICE_ID, PHONE_TESTER
 from wapchita import Wapchita
 
 
-@pytest.mark.skip(reason="Solo pueden eliminarse mensajes en "
-                        "estado queued, por lo que a veces falla")
 def test_delete_message():
     wapchita = Wapchita(
         tkn=WAP_API_KEY,
         device=WAP_DEVICE_ID
     )
-    response = wapchita.send_message(phone=PHONE_TESTER, message=f"Simple text message {datetime.now(tz=UTC)}")
+    response = wapchita.send_message(phone=PHONE_TESTER,
+                                     message=f"Simple text message to be deleted {datetime.now(tz=UTC)}")
     assert response.status_code == 201
-    message_wid = response.json()['id']
+    message_wid = response.json()['waId']
     response = wapchita.delete_message(message_wid=message_wid)
-    assert response.status_code == 200
+    assert response.status_code == 202
