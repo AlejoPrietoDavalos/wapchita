1bb38257a5dd1d44905e8b4df36f217cde5e9a53
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-08-22 18:44:05 -0300

    .

--- wapchita/models/webhook.py
+++ wapchita/models/webhook.py
@@ -19,7 +19,7 @@ class WapWebhookBody(BaseWapMsg):
         return self.data.type
 
     @property
-    def message_id(self) -> str:    # TODO: Habría que renombrarlo a message_wid, pero cambiar también en shock_api.
+    def message_wid(self) -> str:
         return self.id
 
     @property
