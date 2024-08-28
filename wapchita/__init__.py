8b10059909584ff1cead405c52de2c8ebe44cd8a
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-08-27 23:11:21 -0300

    phone2wid en __init__

--- wapchita/__init__.py
+++ wapchita/__init__.py
@@ -5,3 +5,4 @@ from wapchita.models.user import WapUser
 from wapchita.models.chats import WapChat, WapChats
 from wapchita.models.device import WapDevice
 from wapchita.models.webhook import WapWebhookBody
+from wapchita.utils import phone2wid
