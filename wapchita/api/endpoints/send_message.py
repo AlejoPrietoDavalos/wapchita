acac796a3588b789459ae562ec55368fd61a1e6d
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-08-29 17:57:09 -0300

    ResponseSendMessageData

--- wapchita/api/endpoints/send_message.py
+++ wapchita/api/endpoints/send_message.py
@@ -1,9 +1,42 @@
+from typing import Literal
+from datetime import datetime
+
 import requests
 from requests import Response
+from requestsdantic import BaseJSON, BaseResponse
 
 from wapchita.typings import Priority, PRIORITY_DEFAULT
 from wapchita.api.urls import url_send_message
 from wapchita.api.headers import get_headers_app_json
+
+
+class RetryMaxCount(BaseJSON):
+    max: int
+    count: int
+
+
+class ResponseSendMessageData(BaseJSON):
+    id: str
+    waId: str
+    phone: str
+    wid: str
+    status: Literal["queued"] | str                 # FIXME: Ver todos los estados.
+    deliveryStatus: Literal["queued"] | str         # FIXME: Ver todos los estados.
+    createdAt: datetime
+    deliverAt: datetime
+    message: str
+    priority: Priority
+    retentionPolicy: str
+    retry: RetryMaxCount
+    webhookStatus: str
+    device: str
+
+class ResponseSendMessage(BaseResponse):
+    data: ResponseSendMessageData
+
+    @property
+    def status_code(self) -> int:
+        return 201
 
 
 def send_message(
