9c49db620240769bc88011b984b6349bfa4378f5
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-08-29 15:47:14 -0300

    get_headers(tkn=tkn)

--- wapchita/_api/utils.py
+++ wapchita/_api/utils.py
@@ -12,7 +12,7 @@ from wapchita._api.urls import url_get_message
 def wait_msg_sent(*, tkn: str, message_wid: str) -> Response:
     QUEUED = "queued"  # Status de mensaje en cola.
     url = url_get_message(message_wid=message_wid)
-    headers = get_headers(tkn)
+    headers = get_headers(tkn=tkn)
     response = requests.get(url, headers=headers)
     while response.json()["status"] == QUEUED:
         time.sleep(1)
@@ -23,7 +23,7 @@ def wait_msg_sent(*, tkn: str, message_wid: str) -> Response:
 async def async_wait_msg_sent(*, tkn: str, message_wid: str) -> httpx.Response:
     QUEUED = "queued"
     url = url_get_message(message_wid=message_wid)
-    headers = get_headers(tkn)
+    headers = get_headers(tkn=tkn)
 
     async with httpx.AsyncClient() as httpx_client:
         response = await httpx_client.get(url, headers=headers)
