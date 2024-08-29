747163d6723673341ce93bac8b7194b12a900d7c
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-08-29 17:57:41 -0300

    logs a wait_msg_sent

--- wapchita/api/utils.py
+++ wapchita/api/utils.py
@@ -1,5 +1,6 @@
 import asyncio
 import time
+import logging
 
 import httpx
 import requests
@@ -9,14 +10,17 @@ from wapchita.api.headers import get_headers
 from wapchita.api.urls import url_get_message
 
 QUEUED = "queued"  # Status de mensaje en cola.
+logger = logging.getLogger(__name__)
+
 
 def wait_msg_sent(*, tkn: str, message_wid: str, max_tries: int = 120) -> Response:
     url = url_get_message(message_wid=message_wid)
-    headers = get_headers(intentostkn=tkn)
+    headers = get_headers(tkn=tkn)
     response = requests.get(url, headers=headers)
     k = 0
     while response.json()["status"] == QUEUED and k < max_tries:
         k += 1
+        logger.info(f"wait_msg_sent - {message_wid} - {k}/{max_tries}")
         time.sleep(1)
         response = requests.get(url, headers=headers)
     return response
@@ -31,6 +35,7 @@ async def async_wait_msg_sent(*, tkn: str, message_wid: str, max_tries: int = 12
         k = 0
         while response.json()["status"] == QUEUED and k < max_tries:
             k += 1
+            logger.info(f"wait_msg_sent - {message_wid} - {k}/{max_tries}")
             await asyncio.sleep(1)
             response = await httpx_client.get(url, headers=headers)
     return response
