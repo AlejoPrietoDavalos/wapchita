3276f2eca098edb3374966071ba3ebd236b9c113
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-08-29 17:57:22 -0300

    logs base

--- constants.py
+++ constants.py
@@ -1,8 +1,10 @@
 import os
+import logging
 
 from dotenv import load_dotenv
-
 load_dotenv()
+logging.basicConfig(level=os.getenv("LOG_LEVEL"))
+
 
 WAP_API_KEY = os.getenv("WAP_API_KEY")
 WAP_DEVICE_ID = os.getenv("WAP_DEVICE_ID")
