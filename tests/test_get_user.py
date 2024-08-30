b082edaaeaa94f68480ed4ab394644f326f316c2
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-08-30 00:58:40 -0300

    .

--- tests/test_get_user.py
+++ tests/test_get_user.py
@@ -5,4 +5,3 @@ from wapchita import Wapchita, WapUser
 def test_get_user(wapchita: Wapchita) -> None:
     user = wapchita.user_from_phone(phone=PHONE_TESTER)
     assert isinstance(user, WapUser) and user.phone == PHONE_TESTER
- 
